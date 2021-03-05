f1c9e96bb8a6ee4acb791f46e808f6b1e914fe93
Carolina Ale <redrumjedi@gmail.com>
2021-03-04 22:50:22 -0300

    add some generic styles

--- boilerplate/src/components/Main/index.tsx
+++ boilerplate/src/components/Main/index.tsx
@@ -1,6 +1,12 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>n0xt</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="" />
+    <S.Title>N0xt</S.Title>
+    <S.Description>NextJS</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" alt="" />
+  </S.Wrapper>
 )
+
 export default Main
--- boilerplate/src/components/Main/test.tsx
+++ boilerplate/src/components/Main/test.tsx
@@ -6,10 +6,16 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(
-      screen.getByRole('heading', { name: /n0xt/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('heading', { name: /n0xt/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+
+    it('should render the colors correctly', () => {
+      const { container } = render(<Main />)
+
+      expect(container.firstChild).toHaveStyle({
+        'background-color': '#06092b'
+      })
+    })
   })
 })
