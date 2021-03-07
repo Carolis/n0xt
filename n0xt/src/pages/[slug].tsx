01353cd7a73758312a92a26f9266c7cc176a1d7a
Carolina Ale <redrumjedi@gmail.com>
2021-03-07 16:20:01 -0300

    syntax fix

--- n0xt/src/pages/[slug].tsx
+++ n0xt/src/pages/[slug].tsx
@@ -1,10 +1,10 @@
+import { GetStaticProps } from 'next'
 import client from 'graphql/client'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
-import { GetStaticProps } from 'next'
 import { useRouter } from 'next/dist/client/router'
-import PageTemplate from 'templates/Pages'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
 
-export default function Page({ heading, body }) {
+export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
   if (router.isFallback) return null
   return <PageTemplate heading={heading} body={body} />
@@ -22,7 +22,7 @@ export async function getStaticPaths() {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { page } = await client.request(GET_PAGE_BY_SLUG, {
-    slug: `$params?.slug`
+    slug: `${params?.slug}`
   })
 
   if (!page) return { notFound: true }
