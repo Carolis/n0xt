76470380aca2e73542327121361d11c27351bfdd
Carolina Ale <redrumjedi@gmail.com>
2021-03-07 16:34:04 -0300

    add types for GetPageQuery

--- n0xt/src/pages/[slug].tsx
+++ n0xt/src/pages/[slug].tsx
@@ -3,6 +3,7 @@ import client from 'graphql/client'
 import { GET_PAGES, GET_PAGE_BY_SLUG } from 'graphql/queries'
 import { useRouter } from 'next/dist/client/router'
 import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+import { GetPagesQuery } from 'graphql/generated/graphql'
 
 export default function Page({ heading, body }: PageTemplateProps) {
   const router = useRouter()
@@ -11,7 +12,7 @@ export default function Page({ heading, body }: PageTemplateProps) {
 }
 
 export async function getStaticPaths() {
-  const { pages } = await client.request(GET_PAGES, { first: 3 })
+  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
 
   const paths = pages.map(({ slug }) => ({
     params: { slug }
