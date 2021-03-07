f69ddf70a37bad8628144bf856b6001e94c648f3
Carolina Ale <redrumjedi@gmail.com>
2021-03-06 23:51:30 -0300

    add map inline styles

--- n0xt/src/components/Map/index.tsx
+++ n0xt/src/components/Map/index.tsx
@@ -1,12 +1,16 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Map = () => (
-  <MapContainer center={[0,0]} zoom={13} scrollWheelZoom={false}>
+  <MapContainer
+    center={[0, 0]}
+    zoom={3}
+    style={{ height: '100%', width: '100%' }}
+  >
     <TileLayer
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[0,0]}>
+    <Marker position={[0, 0]}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
