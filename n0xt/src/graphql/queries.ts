42fc481860d34d9f46a8d9417b4f13660cf0442a
Carolina Ale <redrumjedi@gmail.com>
2021-03-07 19:03:55 -0300

    add places query and client for dynamic data

--- n0xt/src/graphql/queries.ts
+++ n0xt/src/graphql/queries.ts
@@ -24,3 +24,25 @@ export const GET_PAGE_BY_SLUG = gql`
     }
   }
 `
+
+export const GET_PLACES = gql`
+  query getPlaces {
+    places {
+      id
+      slug
+      name
+      location {
+        latitude
+        longitude
+      }
+      description {
+        html
+      }
+      gallery {
+        url
+        height
+        width
+      }
+    }
+  }
+`
--- n0xt/src/pages/index.tsx
+++ n0xt/src/pages/index.tsx
@@ -1,16 +1,18 @@
-import dynamic from 'next/dynamic'
-import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
-import LinkWrapper from 'components/LinkWrapper'
+import HomeTemplate from 'templates/Home'
+import { MapProps } from 'components/Map'
+import client from 'graphql/client'
+import { GET_PLACES } from 'graphql/queries'
+import { GetPlacesQuery } from 'graphql/generated/graphql'
 
-const Map = dynamic(() => import('components/Map'), { ssr: false })
+export default function Home({ places }: MapProps) {
+  return <HomeTemplate places={places} />
+}
 
-export default function Home() {
-  return (
-    <>
-      <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-label="About" />
-      </LinkWrapper>
-      <Map />
-    </>
-  )
+export const getStaticProps = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES)
+  return {
+    props: {
+      places
+    }
+  }
 }
