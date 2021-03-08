8ab8ec9e934884618a2e2e9764460a9b229cadbe
Carolina Ale <redrumjedi@gmail.com>
2021-03-07 23:03:42 -0300

    add next-seo and its config file

---
+++ n0xt/next-seo.config.ts
@@ -0,0 +1,14 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'whatever.com',
+      site_name: 'Cool Recipes'
+    },
+    twitter: {
+      handle: '@caroliscaroles',
+      site: '@site',
+      cardType: 'summary_large_image'
+    }
+  }
+  
