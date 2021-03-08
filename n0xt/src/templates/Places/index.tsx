8d26cd6fa6a8c12953248db84e7966b031cb7d5a
Carolina Ale <redrumjedi@gmail.com>
2021-03-07 21:11:19 -0300

    add syntax fix at map func

--- n0xt/src/templates/Places/index.tsx
+++ n0xt/src/templates/Places/index.tsx
@@ -20,9 +20,9 @@ export default function PlacesTemplate({ place }: PlacesTemplateProps) {
     <>
       <h1>{place.name}</h1>
       <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
-      {place.gallery.map((image, index) => {
-        ;<img key={`photo-${index}`} src={image.url} alt={place.name} />
-      })}
+      {place.gallery.map((image, index) => (
+        <img key={`photo-${index}`} src={image.url} alt={place.name} />
+      ))}
     </>
   )
 }
